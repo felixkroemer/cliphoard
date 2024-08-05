d68a024db4bcfb70c1e287b6db0f2ec02063e540
Felix Krömer <kroemer.felix@gmail.com>
2024-08-05 23:02:44 +0200

    make items selectable and fix formatting

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,30 @@ const items = ref([
   { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
 ])
 
+const hoveredItemIndex = ref(null)
+const selectedItemIndex = ref(null)
+
 </script>
 
 <template>
   <div class="p-4 bg-neutral-900">
     <ul class="bg-neutral-800 rounded-md overflow-hidden">
-      <ListItem v-for="(item, index) in items" :item="item" :highlighted="index == 0" />
+      <ListItem v-for="(item, index) in items"
+                :item="item"
+                :highlighted="index === selectedItemIndex"
+                :hovered="index === hoveredItemIndex"
+                :index="index"
+                @mouseover="hoveredItemIndex = index"
+                @mouseout="hoveredItemIndex = null"
+                @delete="(i) => {
+                  if (i === selectedItemIndex) {
+                    selectedItemIndex = null;
+                  } else if (i < selectedItemIndex) {
+                    selectedItemIndex--;
+                  }
+                  items.splice(i, 1)
+                }"
+                @click="selectedItemIndex = index" />
     </ul>
   </div>
 </template>
